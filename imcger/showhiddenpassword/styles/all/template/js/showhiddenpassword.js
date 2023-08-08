569e981f48b83bdb01489196f4443d0d439008f1
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2023-08-08 22:15:17 +0200

    Fixed: Width of the password input field

--- imcger/showhiddenpassword/styles/all/template/js/showhiddenpassword.js
+++ imcger/showhiddenpassword/styles/all/template/js/showhiddenpassword.js
@@ -6,21 +6,33 @@
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
-var imcgerPwInput = document.getElementById("password");
+var imcgerPwInput = document.getElementById('password');
 var imcgerPwToggel = '<button id="imcger-pw-toggelbutton" type="button" onclick="imcgerToggelPw(this)"><i class="icon fa-eye fa-fw" aria-hidden="true"></i></button>';
 
 if (imcgerPwInput) {
-	imcgerPwInput.insertAdjacentHTML("afterend", imcgerPwToggel);
+	imcgerPwInput.insertAdjacentHTML('afterend', imcgerPwToggel);
 }
 
 function imcgerToggelPw(button) {
 	let buttonIcon = button.children[0];
 
-	if (imcgerPwInput.type == "password") {
-		imcgerPwInput.type	 = "text";
-		buttonIcon.className = "icon fa-eye-slash fa-fw";
+	if (imcgerPwInput.type == 'password') {
+		imcgerPwInput.type	 = 'text';
+		buttonIcon.className = 'icon fa-eye-slash fa-fw';
 	} else {
-		imcgerPwInput.type	 = "password";
-		buttonIcon.className = "icon fa-eye fa-fw";
+		imcgerPwInput.type	 = 'password';
+		buttonIcon.className = 'icon fa-eye fa-fw';
 	}
 }
+
+function imcgerInitPw() {
+	let inputFieldWidth = parseInt(document.getElementById('username').clientWidth);
+
+	if (inputFieldWidth) {
+		let pwInput = document.getElementById('password');
+		pwInput.classList.remove('autowidth');
+		pwInput.style.width = inputFieldWidth + 'px';
+	}
+}
+
+window.addEventListener('load', imcgerInitPw);
