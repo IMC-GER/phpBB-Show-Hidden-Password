2b0904efd90ecc4b911f50cfa3debe5439c414af
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2023-08-26 22:00:12 +0200

    Update ext.php

--- imcger/showhiddenpassword/ext.php
+++ imcger/showhiddenpassword/ext.php
@@ -14,7 +14,7 @@ class ext extends \phpbb\extension\base
 {
 	public function is_enableable()
 	{
-		$valid_phpbb = phpbb_version_compare(PHPBB_VERSION, '3.3.0', '>=') && phpbb_version_compare(PHPBB_VERSION, '4.0.0', '<');
+		$valid_phpbb = phpbb_version_compare(PHPBB_VERSION, '3.3.0', '>=') && phpbb_version_compare(PHPBB_VERSION, '4.0.0-dev', '<');
 		$valid_php	 = phpbb_version_compare(PHP_VERSION, '7.1.3', '>=');
 
 		return ($valid_phpbb && $valid_php);
