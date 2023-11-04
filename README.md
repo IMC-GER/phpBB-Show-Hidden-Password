203b4a574b96616eebf6c854d89972c817b047ed
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2023-11-04 13:53:33 +0100

    v0.5.1
    - Add listener for Toogle button click event
    - Changed don't toggle password when editing

--- README.md
+++ README.md
@@ -27,6 +27,10 @@ Go to "ACP" > "Customise" > "Manage extensions" and enable the "Show Hidden Pass
 
 ## Changelog
 
+### v0.5.1 (04-11-2023)
+- Minore code change
+- Changed don't toggle password when editing
+
 ### v0.5.0 (03-11-2023)
 - Changed JS code to class
 - Added support for password fields at register
--- imcger/showhiddenpassword/styles/all/template/js/showhiddenpassword.js
+++ imcger/showhiddenpassword/styles/all/template/js/showhiddenpassword.js
@@ -10,12 +10,11 @@
 class imcgerPw {
 
 	// Initialize the form
-	constructor(inputElement, objName) {
+	constructor(inputElement) {
 		var thisObj = this;
 
 		this.inputPwField = inputElement;
-		this.objName = objName;
-		this.inputPwField.insertAdjacentHTML('afterend', '<button id="' + this.inputPwField.id + '_togglebutton" class="imcger-pw-togglebutton" type="button" onclick="' + this.objName + '.togglePw(this)"><i id="fa-eye" class="icon fa-eye fa-fw" aria-hidden="true"></i></button>');
+		this.inputPwField.insertAdjacentHTML('afterend', '<button id="' + this.inputPwField.id + '_togglebutton" class="imcger-pw-togglebutton" type="button"><i id="fa-eye" class="icon fa-eye fa-fw" aria-hidden="true"></i></button>');
 		this.pwToggle = document.getElementById(this.inputPwField.id + '_togglebutton');
 
 		this.inputPaddRight  = window.getComputedStyle(this.inputPwField).getPropertyValue('padding-rigth');
@@ -30,6 +29,10 @@ class imcgerPw {
 
 		window.addEventListener('resize', function () {
 			thisObj.setWidth();
+		});
+
+		this.pwToggle.addEventListener('click', function (e) {
+			thisObj.togglePw(e.target);
 		});
 
 		this.inputPwField.addEventListener('keydown', function () {
@@ -71,7 +74,7 @@ class imcgerPw {
 
 	// Toggle make the password visible
 	togglePw(button) {
-		let buttonIcon = button.children[0];
+		let buttonIcon = button;
 
 		if (this.inputPwField.type == 'password') {
 			this.inputPwField.type = 'text';
@@ -95,10 +98,10 @@ class imcgerPw {
 			this.pwToggleVisible = false;
 			this.pwToggle.style.display = 'none';
 			this.inputPwField.style.paddingRight = this.inputPaddLeft;
-		}
 
-		this.inputPwField.type = 'password';
-		this.pwToggle.firstChild.className = 'icon fa-eye fa-fw';
+			this.inputPwField.type = 'password';
+			this.pwToggle.firstChild.className = 'icon fa-eye fa-fw';
+		}
 	}
 
 	// Set width of password field
@@ -114,13 +117,13 @@ class imcgerPw {
 
 // If the password input fields are available, initialize the form
 if (document.getElementById('password')) {
-	var imcgerPwStart = new imcgerPw(document.getElementById('password'), 'imcgerPwStart');
+	var imcgerPwStart = new imcgerPw(document.getElementById('password'));
 }
 
 if (document.getElementById('new_password')) {
-	var imcgerPwNew = new imcgerPw(document.getElementById('new_password'), 'imcgerPwNew');
+	var imcgerPwNew = new imcgerPw(document.getElementById('new_password'));
 }
 
 if (document.getElementById('password_confirm')) {
-	var imcgerPwConfirm = new imcgerPw(document.getElementById('password_confirm'), 'imcgerPwConfirm');
+	var imcgerPwConfirm = new imcgerPw(document.getElementById('password_confirm'));
 }
